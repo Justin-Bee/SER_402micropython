edb2bd6f7d4e9c92925609c62edf461649f4bc80
Justin-Bee <jbee.appz@gmail.com>
2020-01-28 19:10:13 -0500

    US#254 experimenting with BLE advertising_payload

--- examples/bluetooth/ble_advertising.py
+++ examples/bluetooth/ble_advertising.py
@@ -76,7 +76,7 @@ def decode_services(payload):
 
 
 def demo():
-    payload = advertising_payload(name='micropython', services=[bluetooth.UUID(0x181A), bluetooth.UUID('6E400001-B5A3-F393-E0A9-E50E24DCCA9E')])
+    payload = advertising_payload(name='MicroTyrnkit', services=[bluetooth.UUID(0x181A), bluetooth.UUID('6E400001-B5A3-F393-E0A9-E50E24DCCA9E')])
     print(payload)
     print(decode_name(payload))
     print(decode_services(payload))
--- extmod/modbluetooth_nimble.c
+++ extmod/modbluetooth_nimble.c
@@ -308,7 +308,7 @@ int mp_bluetooth_init(void) {
     while (mp_bluetooth_nimble_ble_state != MP_BLUETOOTH_NIMBLE_BLE_STATE_ACTIVE) {
         MICROPY_EVENT_POLL_HOOK
     }
-
+    ble_svc_gap_device_name_set(MICROPY_PY_BLUETOOTH_DEFAULT_NAME); /* added by Justin Bee US#257 1/26/2020 v1.12.04 */
     return 0;
 }
 
