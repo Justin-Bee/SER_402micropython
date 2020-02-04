e37a49eaac00e2b14e95d8304017edc5624c7787
Justin-Bee <jbee.appz@gmail.com>
2020-02-04 17:19:02 -0500

    US#257 advertising MicroTrynkit

--- ports/esp32/modules/boot.py
+++ ports/esp32/modules/boot.py
@@ -37,72 +37,25 @@ bt= BLE()
 # set active to True initializing the bluetooth module
 bt.active(1)
 
-# Generate a payload to be passed to gap_advertise(adv_data=...).
-def advertising_payload(limited_disc=False, br_edr=False, name=None, services=None, appearance=0):
-    payload = bytearray()
 
-    def _append(adv_type, value):
-        nonlocal payload
-        payload += struct.pack('BB', len(value) + 1, adv_type) + value
-
-    _append(_ADV_TYPE_FLAGS, struct.pack('B', (0x01 if limited_disc else 0x02) + (0x00 if br_edr else 0x04)))
-
-    if name:
-        _append(_ADV_TYPE_NAME, name)
-
-    if services:
-        for uuid in services:
-            b = bytes(uuid)
-            if len(b) == 2:
-                _append(_ADV_TYPE_UUID16_COMPLETE, b)
-            elif len(b) == 4:
-                _append(_ADV_TYPE_UUID32_COMPLETE, b)
-            elif len(b) == 16:
-                _append(_ADV_TYPE_UUID128_COMPLETE, b)
-
-    # See org.bluetooth.characteristic.gap.appearance.xml
-    _append(_ADV_TYPE_APPEARANCE, struct.pack('<h', appearance))
-
-    return payload
-
-def decode_field(payload, adv_type):
-    i = 0
-    result = []
-    while i + 1 < len(payload):
-        if payload[i + 1] == adv_type:
-            result.append(payload[i + 2:i + payload[i] + 1])
-        i += 1 + payload[i]
-    return result
-
-def decode_name(payload):
-    n = decode_field(payload, _ADV_TYPE_NAME)
-    return str(n[0], 'utf-8') if n else ''
+def adv_encode(adv_type, value):
+    return bytes((len(value) + 1, adv_type,)) + value
 
 
-def decode_services(payload):
-    services = []
-    for u in decode_field(payload, _ADV_TYPE_UUID16_COMPLETE):
-        services.append(ubluetooth.UUID(struct.unpack('<h', u)[0]))
-    for u in decode_field(payload, _ADV_TYPE_UUID32_COMPLETE):
-        services.append(ubluetooth.UUID(struct.unpack('<d', u)[0]))
-    for u in decode_field(payload, _ADV_TYPE_UUID128_COMPLETE):
-        services.append(ubluetooth.UUID(u))
-    return services
+def adv_encode_name(name):
+    return adv_encode(const(0x09), name.encode())
 
-payload = bytearray()
-payload = advertising_payload(services=[ubluetooth.UUID(0x1812), ubluetooth.UUID('6E400001-B5A3-F393-E0A9-E50E24DCCA9E')])
-
-
-print(payload)
-# set gap_advertise(interval, adv_data?)
-bt.gap_advertise(100000) #need to figure out why its not displaying
 
 # sest the UUID for our device
 #0x1825 is the SIG service for Object Transfer Service
-_adv_service = ubluetooth.UUID(0x1825)
+#_adv_service = ubluetooth.UUID(0x1825)
 
 #start the gatt service
-bt.gatts_register_services(_adv_service)
+#bt.gatts_register_services(_adv_service, ubluetooth.FLAG_WRITE)
+
+#cannot advertise until after registering services
+# set gap_advertise(interval, adv_data?)
+bt.gap_advertise(100000, adv_encode_name('MicroTrynkit'))  #works correctly
 
 #gatt s_registerservice()
 
