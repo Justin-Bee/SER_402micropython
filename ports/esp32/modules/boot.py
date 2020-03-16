657806b065be6de6ddc14ff40cde4ef01f4c00ab
Justin <jbee.appz@gmail.com>
2020-03-16 14:28:57 -0400

    US#354 finished

--- ports/esp32/modules/boot.py
+++ ports/esp32/modules/boot.py
@@ -72,7 +72,7 @@ def bt_irq(event, data):
         stop_timer()
         timeCheck = False
         #for testing purposes I had to remove the reset to connect with nrfConnect sniffer app
-       # machine.reset()
+        machine.reset()
     elif event == _IRQ_GATTS_WRITE:
         print("IRQ_GATTS_WRITE")
         timer()
@@ -138,9 +138,7 @@ _adv_RX_service = (ubluetooth.UUID('fbdf3e86-c18c-4e5b-aace-e7cc03257f7c'), ublu
 #including the TX and RX characteristics created above.
 _my_service = ((_adv_service, (_adv_TX_service, _adv_RX_service,),),)
 
-#start the gatt service
-# tx_handle is for the TX service to be used for the reads
-((tx_handle, rx_handle),)= bt.gatts_register_services(_my_service)
+
 
 #new service for the serial communication
 # User Data service 0x181C
@@ -149,7 +147,11 @@ _serial_TX_service = (ubluetooth.UUID('f05d9919-02e3-4414-9cbc-5485e0af77d2'), u
 _serial_RX_service = (ubluetooth.UUID('72f235e0-fb1c-4772-96f1-d55a445d5c89'), ubluetooth.FLAG_WRITE,)
 
 _my_serial_service = ((_serial_service,(_serial_TX_service, _serial_RX_service,),),)
-((tx_handle, rx_handle),)= bt.gatts_register_services(_my_serial_service)
+((_tx_handle, _rx_handle),)= bt.gatts_register_services(_my_serial_service)
+
+#start the gatt service
+# tx_handle is for the TX service to be used for the reads
+((tx_handle, rx_handle),)= bt.gatts_register_services(_my_service)
 
 # increase the size of the buffer, default is 20 bytes
 #need to ensure we pick something that is big enough for file transfers
