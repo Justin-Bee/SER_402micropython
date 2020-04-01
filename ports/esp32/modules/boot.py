422816889232b90cae059af20734c4c3c0a421f4
Justin <jbee.appz@gmail.com>
2020-04-01 17:20:41 -0400

    US#393 changing the implementation of the bluetooth

--- ports/esp32/modules/boot.py
+++ ports/esp32/modules/boot.py
@@ -15,12 +15,9 @@ from bluetooth import BLE
 import machine
 import time
 import os
-import io
-import micropython
-
 from micropython import const
 import struct
-from ble_uart_peripheral import BLEUART
+from ble_advertising import advertising_payload
 
 # Advertising payloads are repeated packets of the following form:
 #   1 byte data length (N + 1)
@@ -59,20 +56,50 @@ _IRQ_GATTC_INDICATE                  = const(1 << 14)
 time_after = time.time()
 time_before = time.time()
 timeCheck = False
-global conn_handle
 
+# set the UUID for the GATT Service
+_adv_service = bluetooth.UUID(0x1825)
 
-# create BLE variable
-bt= BLE()
+#UUID for the TX characteristic - 30ff6dae-fbfe-453b-8a99-9688fea23832
+#UUID created by https://www.uuidgenerator.net/version4
+#set the ubluetooth flag for read
+_adv_TX_service = (bluetooth.UUID('30ff6dae-fbfe-453b-8a99-9688fea23832'), bluetooth.FLAG_NOTIFY,)
 
-# set active to True initializing the bluetooth module
-bt.active(1)
+#UUID for the RX characteristic - fbdf3e86-c18c-4e5b-aace-e7cc03257f7c
+#UUID created by https://www.uuidgenerator.net/version4
+#set the ubluetooth flag for write
+_adv_RX_service = (bluetooth.UUID('fbdf3e86-c18c-4e5b-aace-e7cc03257f7c'), bluetooth.FLAG_WRITE,)
+
+# MicroTrynkit Service
+#including the TX and RX characteristics created above.
+_my_service = (_adv_service, (_adv_TX_service, _adv_RX_service,),)
+
+# org.bluetooth.characteristic.gap.appearance.xml
+_ADV_APPEARANCE_GENERIC_COMPUTER = const(128)
 
 # adding banner message to the device
 print("****************************************************")
 print("* SER 402 - Project 5 Trynkit                      *")
 print("* Micropython on ESP32                             *")
 print("****************************************************")
+
+
+class MICROTRYNKIT:
+    def __init__(self, ble, name='MicroTrynkit'):
+        self._ble = ble
+        self._ble.active(True)
+        self._ble.irq(handler=self.bt_irq)
+        ((self._tx_handle, self._rx_handle,),) = self._ble.gatts_register_services((_my_service,))
+        # Increase the size of the rx buffer and enable append mode.
+        self._connections = set()
+        self._rx_buffer = bytearray()
+        self._handler = None
+        # Optionally add services=[_UART_UUID], but this is likely to make the payload too large.
+        self._payload = advertising_payload(name=name, appearance=_ADV_APPEARANCE_GENERIC_COMPUTER)
+        self._advertise()
+
+    def irq(self, handler):
+        self._handler = handler
 
 ############################################################
 #
@@ -84,48 +111,54 @@ print("****************************************************")
 # IRQ_CENTRAL_DISCONNECT - handles the disconnection and reboots the device
 #
 #############################################################
-def bt_irq(event, data):
-    global timeCheck
-    if event == _IRQ_CENTRAL_CONNECT:
-        print("IRQ_CENTRAL_CONNECT")
-        conn_handle, addr_type, addr = data
-    elif event == _IRQ_CENTRAL_DISCONNECT:
-        print("IRQ_CENTRAL_DISCONNECT")
-        stop_timer()
-        timeCheck = False
-        #for testing purposes I had to remove the reset to connect with nrfConnect sniffer app
-        machine.reset()
-    elif event == _IRQ_GATTS_WRITE:
-        print("IRQ_GATTS_WRITE")
-        timer()
-        x = bt.gatts_read(rx_handle)
-        temp = x.decode('utf-8')
-        if(temp == 'erase'):
-            # check if main.py exists in the flash memory
-            if("main.py" in os.listdir()):
-                # since the file already exists, erase it so it can start new
-                os.remove('main.py')
-        else:
-            # add newline char to end of the line that was sent
-            x = x + '\n'
-            # create a new file or append to existing
-            f = open('main.py', 'a')
-            #write to the file
-            f.write(x)
-            #close the file
-            f.close()
-        tx_handle = 'Upload finished'
-    elif event == _IRQ_GATTS_READ_REQUEST:
-        print("IRQ_GATTS_READ_REQUEST")
-        x = 'test'
-        tx_handle = x.encode('utf-8')
-    elif event == _IRQ_GATTC_NOTIFY:
-        print("IRQ_GATTC_NOTIFY")
-        x = "TEST"
-        tx_handle = x.encode('utf-8')
+    def bt_irq(self, event, data):
+        global timeCheck
+        if event == _IRQ_CENTRAL_CONNECT:
+            print("IRQ_CENTRAL_CONNECT")
+            conn_handle, _, _ = data
+            self._connections.add(conn_handle)
+        elif event == _IRQ_CENTRAL_DISCONNECT:
+            print("IRQ_CENTRAL_DISCONNECT")
+            stop_timer()
+            timeCheck = False
+            #for testing purposes I had to remove the reset to connect with nrfConnect sniffer app
+            machine.reset()
+        elif event == _IRQ_GATTS_WRITE:
+            print("IRQ_GATTS_WRITE")
+            timer()
+            x = self._ble.gatts_read(self._rx_handle)
+            temp = x.decode('utf-8')
+            if(temp == 'erase'):
+                # check if main.py exists in the flash memory
+                if("main.py" in os.listdir()):
+                    # since the file already exists, erase it so it can start new
+                    os.remove('main.py')
+            else:
+                # add newline char to end of the line that was sent
+                x = x + '\n'
+                # create a new file or append to existing
+                f = open('main.py', 'a')
+                #write to the file
+                f.write(x)
+                #close the file
+                f.close()
+            tx_handle = 'Upload finished'
+        elif event == _IRQ_GATTS_READ_REQUEST:
+            print("IRQ_GATTS_READ_REQUEST")
+            x = 'test'
+            tx_handle = x.encode('utf-8')
+        elif event == _IRQ_GATTC_NOTIFY:
+            print("IRQ_GATTC_NOTIFY")
+            x = "TEST"
+            tx_handle = x.encode('utf-8')
 
+    def write(self, data):
+        for conn_handle in self._connections:
+            self._ble.gatts_notify(conn_handle, self._tx_handle, data)
 
-#bt.irq(bt_irq)
+    def _advertise(self, interval_us=500000):
+        self._ble.gap_advertise(interval_us, adv_data=self._payload)
+
 
 # timer() - this function gets the time at the beginning of the upload
 def timer():
@@ -151,111 +184,17 @@ def adv_encode(adv_type, value):
 def adv_encode_name(name):
     return adv_encode(_ADV_TYPE_NAME, name.encode())
 
-# set the UUID for the GATT Service
-_adv_service = bluetooth.UUID(0x1825)
 
-#UUID for the TX characteristic - 30ff6dae-fbfe-453b-8a99-9688fea23832
-#UUID created by https://www.uuidgenerator.net/version4
-#set the ubluetooth flag for read
-_adv_TX_service = (bluetooth.UUID('30ff6dae-fbfe-453b-8a99-9688fea23832'), bluetooth.FLAG_READ)
-
-#UUID for the RX characteristic - fbdf3e86-c18c-4e5b-aace-e7cc03257f7c
-#UUID created by https://www.uuidgenerator.net/version4
-#set the ubluetooth flag for write
-_adv_RX_service = (bluetooth.UUID('fbdf3e86-c18c-4e5b-aace-e7cc03257f7c'), bluetooth.FLAG_WRITE,)
-
-# MicroTrynkit Service
-#including the TX and RX characteristics created above.
-_my_service = ((_adv_service, (_adv_TX_service, _adv_RX_service,),),)
-
-#start the gatt service
-# tx_handle is for the TX service to be used for the reads
-((tx_handle, rx_handle),)= bt.gatts_register_services(_my_service)
+ble = bluetooth.BLE()
+bt = MICROTRYNKIT(ble)
 
 # increase the size of the buffer, default is 20 bytes
 #need to ensure we pick something that is big enough for file transfers
-bt.gatts_write(rx_handle, bytes(1024))
-
-#gap_advertise()
-#params: interval, adv_data
-bt.gap_advertise(100000, adv_encode_name('MicroTrynkit'))
-
-
-#this function sends to data from esp32 to be read from connected device. Just for testing purposes now.
-bt.gatts_write(tx_handle, str.encode("hopefully this works"))
+#bt.gatts_write(rx_handle, bytes(1024))
 
 #adding gatt notify
-#bt.gatts_notify(conn_handle, 1)
+bt.write("TESTING 1,2,3....")
 
-_MP_STREAM_POLL = const(3)
-_MP_STREAM_POLL_RD = const(0x0001)
-
-# TODO: Remove this when STM32 gets machine.Timer.
-if hasattr(machine, 'Timer'):
-    _timer = machine.Timer(-1)
-else:
-    _timer = None
-
-# Batch writes into 50ms intervals.
-def schedule_in(handler, delay_ms):
-    def _wrap(_arg):
-        handler()
-    if _timer:
-        _timer.init(mode=machine.Timer.ONE_SHOT, period=delay_ms, callback=_wrap)
-    else:
-        micropython.schedule(_wrap, None)
-
-# Simple buffering stream to support the dupterm requirements.
-class BLEUARTStream(io.IOBase):
-    def __init__(self, uart):
-        self._uart = uart
-        self._tx_buf = bytearray()
-        self._uart.irq(self._on_rx)
-
-    def _on_rx(self):
-        # Needed for ESP32.
-        if hasattr(os, 'dupterm_notify'):
-            os.dupterm_notify(None)
-
-    def read(self, sz=None):
-        return self._uart.read(sz)
-
-    def readinto(self, buf):
-        avail = self._uart.read(len(buf))
-        if not avail:
-            return None
-        for i in range(len(avail)):
-            buf[i] = avail[i]
-        return len(avail)
-
-    def ioctl(self, op, arg):
-        if op == _MP_STREAM_POLL:
-            if self._uart.any():
-                return _MP_STREAM_POLL_RD
-        return 0
-
-    def _flush(self):
-        data = self._tx_buf[0:100]
-        self._tx_buf = self._tx_buf[100:]
-        self._uart.write(data)
-        if self._tx_buf:
-            schedule_in(self._flush, 50)
-
-    def write(self, buf):
-        empty = not self._tx_buf
-        self._tx_buf += buf
-        if empty:
-            schedule_in(self._flush, 50)
-
-
-def start():
-   # ble = bluetooth.BLE()
-    uart = BLEUART(bt, name='MicroTrynkit')
-    stream = BLEUARTStream(uart)
-
-    os.dupterm(stream)
-
-#start()
 
 #END OF FILE
 ########################################################################################################################
